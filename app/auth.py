16f01109e483720cc1c5884e95a99e3ba82971ef
Matheus Tomaz Souza <51293473+mathtomaz26@users.noreply.github.com>
2025-06-04 16:17:26 -0300

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,7 @@
 from fastapi import Header, HTTPException
 import os
 
-def validar_token(authorization: str = Header(...)):
+def validar_token(authorization: str = Header(..., alias="Authorization")):
     token_esperado = f"Bearer {os.getenv('API_TOKEN')}"
     if authorization != token_esperado:
         raise HTTPException(status_code=401, detail="Token inválido")
